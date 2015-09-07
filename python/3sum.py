7f577a8a8e6e1521329c7d8e3567f6671debbf27
SMTech <yellow-ming@163.com>
2015-09-07 19:06:28 +0800

    first commit

---
+++ python/3sum.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        
+        # ATTENTIONS!
+        # Pass duplicte item to speed up algorithm!
+        
+        n = len(nums)
+        if n < 3:
+            return []
+        
+        # sort list. n*log n
+        nums.sort()
+        
+        triplets = list() #output list
+        triplet = list()  #triple
+        
+        s = 0
+        low = 0  # two pointers
+        high = 0
+        for i in xrange(n-2):
+            if i>0 and nums[i] == nums[i-1]:  #duplicate
+                continue
+            low = i+1
+            high = n-1
+            while low < high:
+                s = nums[i] + nums[low] + nums[high]
+                if s == 0:
+                    triplet = [nums[i],nums[low],nums[high]]
+                    triplets.append(triplet)
+                    while low < high and nums[low] == nums[low+1]: #duplicate
+                        low += 1
+                    while low < high and nums[high] == nums[high-1]:
+                        high -= 1
+                if s < 0:
+                    low += 1
+                else:
+                    high -= 1
+                
+                
+        return triplets

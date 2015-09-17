bec7bd8017798b55b0c193cd24d4a632cc415e4d
yunyu-Mr <yellow-ming@163.com>
2015-09-17 09:39:49 +0800

    --

--- python/next_permutation.py
+++ python/next_permutation.py
@@ -12,14 +12,15 @@ class Solution(object):
         while nums[i] <= nums[j] and j >=0:
             j -= 1
             i -= 1
+
         if j >= 0:
             i = len(nums) -1
-            while nums[j] >= nums[i]:
+            while nums[j] >= nums[i]:  # find a smallest num on right but bigger than nums[j] (merely bigger than nums[j])
                 i -= 1
             nums[i], nums[j] = nums[j], nums[i]
             k = j +1
         else:
-            k = 0
+            k = 0  # if j ==-1, reverse all
         
         i = len(nums) -1
         while k < i:

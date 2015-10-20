9d4cdc2ef21f8803876a6a004e67c6161adf08ce
SMTech <yellow-ming@163.com>
2015-10-20 11:54:59 +0800

    add method to kth...

--- python/kth_largest_element.py
+++ python/kth_largest_element.py
@@ -1,4 +1,5 @@
 import heapq
+import random
 class Solution(object):
     def findKthLargest(self, nums, k):
         """
@@ -16,6 +17,49 @@ class Solution(object):
         # Return the kth element
         return -heapq.heappop(heap)
 
+class Solution2(object):
+    """
+    Method 2: Divide and Conquer
+    """
+    def findKthLargest(self, nums, k):
+        return self.randomSelect(nums, k, 0, len(nums)-1)
+
+    def partition(self, nums, p, r):
+        i, j = p+1, r
+        x = nums[p]  # pivot value
+        while True:
+            while nums[i] < x and i < r:
+                i += 1
+            while nums[j] > x:
+                j -= 1
+            if i >= j:
+                break
+            # swap, small to left
+            nums[i], nums[j] = nums[j], nums[i]
+        # nums[j] <= x
+        nums[p], nums[j] = nums[j], x
+        return j
+
+    def randomPartitoin(self, nums, p, r):
+        # partition nums
+        s = random.randint(p, r)
+        nums[p], nums[s] = nums[s], nums[p]
+        return self.partition(nums, p, r)
+
+    def randomSelect(self, nums, k, p, r):
+        if p == r:
+            return nums[p]
+        i = self.randomPartitoin(nums, p, r)
+        n_small = i - p + 1  # number of left part
+        if k > n_small:
+            return self.randomSelect(nums, k-n_small, i+1, r)
+        return self.randomSelect(nums, k, p, i)
+
 if __name__ == '__main__':
-    s = Solution()
-    print s.findKthLargest([0,1,2,3,4,5,6], 2)
+    # s = Solution()
+    # print s.findKthLargest([0,1,2,3,4,5,6], 2)
+    s = Solution2()
+    nums = [4,5,1,7,2,3]
+    # s.partition(nums, 0, len(nums)-1)
+    print s.findKthLargest([99,99], 1)
+    print nums

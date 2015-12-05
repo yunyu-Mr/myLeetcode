8754756df0e6db3ec4cdbb2a70195172dedb2a43
SMTech <yellow-ming@163.com>
2015-12-05 11:27:36 +0800

    add linked list cycle 2

--- cpp/linked_list_cycle_2.cpp
+++ cpp/linked_list_cycle_2.cpp
@@ -1,6 +1,31 @@
+/*
+* Linked List Cycle 2
+*/
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        
+        // Using hash set to store checked ListNode
+		unordered_set<ListNode*> listSet;
+		while (head != nullptr) {
+			// if the node has already been checked, there's a loop
+			if (listSet.find(head) != listSet.end())
+				return head; // return where the cycle begin
+			else
+				listSet.insert(head);
+			head = head->next;
+		}
+		return nullptr;
     }
 };

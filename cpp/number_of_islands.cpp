4048c100e41c31a0de6a2d05fdc9b3d640f9ff69
smtech <yellow-ming@163.com>
2016-04-25 14:32:40 +0800

    number of island. add comment

--- cpp/number_of_islands.cpp
+++ cpp/number_of_islands.cpp
@@ -8,20 +8,26 @@ using namespace std;
 class Solution {
 private:
     int m, n;
+    // Queue for BFS. Store vetex.
     queue<pair<int,int> > Q;
+    // Check whether adjacent points are land or water?
     void enqueue(int i, int j, vector<vector<char> >& grid, vector<vector<bool> >&color) {
+        // Left
         if (i > 0 && grid[i-1][j] == '1' && color[i-1][j] == 0) {
             color[i-1][j] = 1;
             Q.push(make_pair(i-1, j));
         }
+        // Right
         if (i < m-1 && grid[i+1][j] == '1' && color[i+1][j] == 0){
              color[i+1][j] = 1;
              Q.push(make_pair(i+1, j));
         }
+        // Below
         if (j < n-1 && grid[i][j+1] == '1' && color[i][j+1] == 0){
              color[i][j+1] = 1;
              Q.push(make_pair(i, j+1));
          }
+         // Upper
          if (j > 0 && grid[i][j-1] == '1' && color[i][j-1] == 0){
              color[i][j-1] = 1;
              Q.push(make_pair(i, j-1));
@@ -37,22 +43,21 @@ public:
             return 0;
             
         int num = 0;
-        // queue<pair<int,int> > Q;
         vector<vector<bool> >  color(m, vector<bool>(n, 0));  // 2d array
         
         for (int i = 0; i < m; ++i) 
             for (int j = 0; j < n; ++j)
                 if (grid[i][j] == '1' && color[i][j] == 0) {
+                    // Breath first search. Expand and mark a island.
                     color[i][j] = 1;
                     Q.push(make_pair(i,j));
                     while (!Q.empty()) {
                         pair<int,int> v = Q.front(); Q.pop();
                         enqueue(v.first, v.second, grid, color);
                     }
+                    // One island found.
                     num++;
                 }
-             
-        
         return num;
     }
 };

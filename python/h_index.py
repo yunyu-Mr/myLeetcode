fefea6bd1396b19b8b4d7e2058a9b691f5922a06
smtech <yellow-ming@163.com>
2016-05-07 18:08:53 +0800

    add h index

---
+++ python/h_index.py
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*-  
+"""
+H-Index
+
+A scientist has index h if h of his/her N papers have at least h citations each, 
+and the other N − h papers have no more than h citations each.
+"""
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        O(n * log n)
+        """
+        # Sort the citations list.
+        citations.sort()
+        
+        # Check from large to small.
+        h = 0
+        for idx in citations[::-1]:
+            if idx > h:
+                h += 1
+            else :
+                break
+        return h
+        
+if __name__ == '__main__':
+    s = Solution()
+    print s.hIndex([3, 0, 6, 1, 5])
